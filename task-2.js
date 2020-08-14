4c312649cc8c405bd65cdb78281582059a4cff50
abakarovmaks <abakarovmaks@gmail.com>
2020-08-14 13:20:30 +0300

    Решает задачу 2

---
+++ task-2.js
@@ -0,0 +1,10 @@
+// 1 Переменные
+const total = 100;
+const ordered = 50;
+// 2 если ordered > total
+
+if (ordered >= total) {
+  console.log("На складе недостаточно товаров");
+} else {
+  console.log("Заказ оформлен, с вами свяжется менеджер");
+}
