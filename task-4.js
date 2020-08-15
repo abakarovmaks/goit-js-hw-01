901bed8ad38c98a4cdc3815fa6452ad8f11d4c09
abakarovmaks <abakarovmaks@gmail.com>
2020-08-15 14:07:49 +0300

    Решает задачу 4

--- task-4.js
+++ task-4.js
@@ -1,35 +1,16 @@
+const credits = 23580;
+const pricePerDroid = 3000;
 
-// Использование if else
-// Напиши скрипт имитирующий авторизацию администратора в панели управления.
+const purchase = prompt("Какое количество дроидов Вы хотите купить?");
+console.log(purchase);
+let totalPrice = purchase * pricePerDroid;
+let balanceCredit = credits - totalPrice;
+let message;
 
-// Есть переменная message в которую надо будет записать одно из трех сообщений о результате:
+if (purchase === null) {
+  message = "Отменено пользователем!";
+} else if (balanceCredit > -1) {
+  message = `Вы купили ${purchase} дроидов, на счету осталось ${balanceCredit} кредитов.`;
+} else message = "Недостаточно средств на счету!";
 
-// const CANCELED_BY_USER = 'Отменено пользователем!'
-// const ACCESS_IS_ALLOWED = 'Добро пожаловать!'
-// const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!
-// Выбор одного из трех значений зависит от того, какое значение будет у переменной userPassword (данные, которые может передать пользователь). Возможны такие варианты:
-
-// const ADMIN_PASSWORD = 'jqueryismyjam'. При таком значении userPassword переменной message следует присвоить ACCESS_IS_ALLOWED.
-// null. При таком значении userPassword переменной message следует присвоить CANCELED_BY_USER.
-// Любая строка, кроме ADMIN_PASSWORD. При таком значении userPassword переменной message следует присвоить ACCESS_DENIED.
-// Используйте if else if else для написания скрипта.
-
-// 1 Переменные 
-const message = 
-const userPassword = 
-
-// 2 Выбор переменной
-const CANCELED_BY_USER = 'Отменено пользователем!'
-const ACCESS_IS_ALLOWED = 'Добро пожаловать!'
-const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!'
-
-// 3 Переменные, которые передаёт пользователь
-const ADMIN_PASSWORD = 'jqueryismyjam'. // При таком значении userPassword переменной message следует присвоить ACCESS_IS_ALLOWED.
-
-null // При таком значении userPassword переменной message следует присвоить CANCELED_BY_USER.
-
-// Любая строка, кроме ADMIN_PASSWORD. При таком значении userPassword переменной message следует присвоить ACCESS_DENIED.
-
-// Используйте if else if else для написания скрипта.
-
-
+console.log(message);
